e62ad9f455ec5b74dbe62994f0b3c7d8659b2cd5
Nit4850 <nitesh10048@gmail.com>
2023-02-18 16:17:53 +0530

    change 1

--- src/main/java/com/driver/controller/BookController.java
+++ src/main/java/com/driver/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.driver.controller;
 
 import com.driver.models.Book;
+import com.driver.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +11,22 @@ import java.util.List;
 
 //Add required annotations
 
+@RestController
+@RequestMapping("/book")
 public class BookController {
 
+    @Autowired
+    BookService bookService;
 
     //Write createBook API with required annotations
+    @PostMapping("/create")
+    public ResponseEntity<String> createBook(Book book){
+        bookService.createBook(book);
+        return new ResponseEntity<>("Success",HttpStatus.ACCEPTED);
+    }
 
     //Add required annotations
+    @GetMapping("/get_book")
     public ResponseEntity getBooks(@RequestParam(value = "genre", required = false) String genre,
                                    @RequestParam(value = "available", required = false, defaultValue = "false") boolean available,
                                    @RequestParam(value = "author", required = false) String author){
--- src/main/java/com/driver/controller/StudentController.java
+++ src/main/java/com/driver/controller/StudentController.java
@@ -1,37 +1,49 @@
 package com.driver.controller;
 
 import com.driver.models.Student;
+import com.driver.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //Add required annotations
+@RestController
+@RequestMapping("/student")
 public class StudentController {
 
     //Add required annotations
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping("get_by_mail")
     public ResponseEntity getStudentByEmail(@RequestParam("email") String email){
         return new ResponseEntity<>("Student details printed successfully ", HttpStatus.OK);
     }
 
     //Add required annotations
+    @GetMapping("/get_by_id")
     public ResponseEntity getStudentById(@RequestParam("id") int id){
 
         return new ResponseEntity<>("Student details printed successfully ", HttpStatus.OK);
     }
 
     //Add required annotations
+    @PostMapping("/create")
     public ResponseEntity createStudent(@RequestBody Student student){
 
         return new ResponseEntity<>("the student is successfully added to the system", HttpStatus.CREATED);
     }
 
     //Add required annotations
+    @PutMapping("/update")
     public ResponseEntity updateStudent(@RequestBody Student student){
 
         return new ResponseEntity<>("student is updated", HttpStatus.ACCEPTED);
     }
 
     //Add required annotations
+    @DeleteMapping("/delete")
     public ResponseEntity deleteStudent(@RequestParam("id") int id){
 
         return new ResponseEntity<>("student is deleted", HttpStatus.ACCEPTED);
